7836437925d6653f3bfb96dd14ae0bf035b78939
erik antony <erik.antony,mg@gmail.com>
2025-04-10 17:37:15 -0500

    completar ingredientes y receta en menu

--- src/app/menu/services/plato.service.ts
+++ src/app/menu/services/plato.service.ts
@@ -3,35 +3,39 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@env/environment';
 import { IPlato } from '../models';
+import { IMenuRecetaIngredientes } from '../models/menu.platos.receta';
 
 @Injectable({
-    providedIn: 'root'
-  })
-  export class PlatoHttpService {
-    private apiUrl: string;
-  
-    constructor(private http: HttpClient) {
-      this.apiUrl = environment.apiUrl;
-    }
-    
-    getAll(): Observable<IPlato[]> {
-      return this.http.get<IPlato[]>(`${this.apiUrl}/plato`);
-    }
-  
-    getById(id: number): Observable<IPlato> {
-      return this.http.get<IPlato>(`${this.apiUrl}/plato/${id}`);
-    }
-  
-    add(plato: IPlato): Observable<IPlato> {
-      return this.http.post<IPlato>(`${this.apiUrl}/plato`, plato);
-    }
+  providedIn: 'root',
+})
+export class PlatoHttpService {
+  private apiUrl: string;
 
-    update(id: number, plato: IPlato): Observable<IPlato> {
-      return this.http.put<IPlato>(`${this.apiUrl}/plato/${id}`, plato);
-    }
-  
-    delete(id: number): Observable<void> {
-      return this.http.delete<void>(`${this.apiUrl}/plato/${id}`);
-    }
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl;
   }
-  
+
+  getAll(): Observable<IPlato[]> {
+    return this.http.get<IPlato[]>(`${this.apiUrl}/plato`);
+  }
+  getRecetaByMenu(id: number): Observable<IMenuRecetaIngredientes[]> {
+    return this.http.get<IMenuRecetaIngredientes[]>(
+      `${this.apiUrl}/plato/${id}/receta`
+    );
+  }
+  getById(id: number): Observable<IPlato> {
+    return this.http.get<IPlato>(`${this.apiUrl}/plato/${id}`);
+  }
+
+  add(plato: IPlato): Observable<IPlato> {
+    return this.http.post<IPlato>(`${this.apiUrl}/plato`, plato);
+  }
+
+  update(id: number, plato: IPlato): Observable<IPlato> {
+    return this.http.put<IPlato>(`${this.apiUrl}/plato/${id}`, plato);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/plato/${id}`);
+  }
+}
